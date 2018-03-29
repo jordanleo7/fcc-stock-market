5cb0f8d15fdc6e0b915580c1b2a16c861cbdaeaf
Jordan Johnson <jordanleo7@hotmail.com>
2018-03-28 19:51:46 -0500

    Added IEX ToS to Home.js temporarily

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,16 @@ class Home extends Component {
     return (
       <div>
         <StockList />
+        <footer>
+          <div>
+            <p>
+              Data provided for free by <a href="https://iextrading.com/developer">IEX</a>.
+            </p>
+            <p>
+              IEX Terms of Service: <a href="https://iextrading.com/api-exhibit-a">IEX Exhibit A</a>
+            </p>
+          </div>
+        </footer>
       </div>
     )
   }
